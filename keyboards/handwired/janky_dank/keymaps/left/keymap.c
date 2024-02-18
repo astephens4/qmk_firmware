0592b1dd1fedfa61b002aa75a782c568b8af113c
Alex Stephens <a.stephens4@gmail.com>
2024-02-18 00:01:28 -0500

    Keymap is more like what I use

--- keyboards/handwired/janky_dank/keymaps/left/keymap.c
+++ keyboards/handwired/janky_dank/keymaps/left/keymap.c
@@ -13,9 +13,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_4x8(
-        KC_TAB,         KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,       _______,       _______,   
-        KC_CAPS,        KC_A,       KC_R,       KC_S,       KC_T,       KC_G,       _______,       _______,   
-        KC_LCTL,        KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,       KC_K,       KC_H,   
-        _______,        _______,    KC_ESC,     KC_BSPC,    KC_DEL,     _______,    KC_END,     KC_ENT
+        KC_TAB,         KC_Q,       KC_W,           KC_F,           KC_P,       KC_B,       _______,        _______,   
+        KC_CAPS,        KC_A,       KC_R,           KC_S,           KC_T,       KC_G,       _______,        _______,   
+        KC_LCTL,        KC_Z,       KC_X,           KC_C,           KC_D,       KC_V,       KC_SPACE,       KC_HOME,   
+        _______,        _______,    KC_LEFT_ALT,    KC_LEFT_GUI,    KC_ESC,     _______,    KC_BSPC,        KC_DEL
     )
 };
