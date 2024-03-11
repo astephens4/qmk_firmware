9d712efae8ad19eea1f8e3027f33f4204d927463
Alex Stephens <a.stephens4@gmail.com>
2024-03-10 23:40:54 -0400

    Added license header to keymap files

--- keyboards/handwired/janky_dank/keymaps/via/keymap.c
+++ keyboards/handwired/janky_dank/keymaps/via/keymap.c
@@ -1,3 +1,20 @@
+/*
+Copyright 2024 Alex Stephens
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include QMK_KEYBOARD_H
 
 enum Layers
@@ -22,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_4x8(
         KC_TAB,                 KC_Q,    KC_W,        KC_F,        KC_P,   KC_B,    XXXXXXX,  XXXXXXX,                      /**/ XXXXXXX,               XXXXXXX,  KC_J,    KC_L,   KC_U,         KC_Y,         KC_SEMICOLON, KC_BACKSLASH,
         LT(SYMNAV, KC_CAPS),    KC_A,    KC_R,        KC_S,        KC_T,   KC_G,    XXXXXXX,  XXXXXXX,                      /**/ XXXXXXX,               XXXXXXX,  KC_M,    KC_N,   KC_E,         KC_I,         KC_O,         KC_QUOTE,
-        KC_LCTL,                KC_Z,    KC_X,        KC_C,        KC_D,   KC_V,    KC_HOME, KC_SPACE,                      /**/ KC_SPACE,              KC_END,   KC_K,    KC_H,   KC_COMMA,     KC_DOT,       KC_SLASH,     KC_MINUS,
-        XXXXXXX,                XXXXXXX, KC_LEFT_ALT, KC_ESC,      KC_BSPC, XXXXXXX, SFT_T(KC_DEL), LT(NUMPAD, KC_EQUAL),   /**/ LT(NUMPAD, KC_ESC),    SFT_T(KC_ENT), XXXXXXX, KC_SPACE, KC_RIGHT_GUI, KC_RIGHT_ALT, XXXXXXX,      XXXXXXX
+        KC_LCTL,                KC_Z,    KC_X,        KC_C,        KC_D,   KC_V,    KC_HOME, KC_SPACE,                      /**/ KC_SPACE,              KC_END,   KC_K,    KC_H,   KC_COMMA,     KC_DOT,       KC_SLASH,     KC_RCTL,
+        XXXXXXX,                XXXXXXX, KC_LEFT_ALT, KC_ESC,      KC_BSPC, XXXXXXX, SFT_T(KC_DEL), LT(NUMPAD, KC_EQUAL),   /**/ LT(NUMPAD, KC_ESC),    SFT_T(KC_ENT), XXXXXXX, KC_SPACE, KC_LEFT_GUI, KC_RIGHT_ALT, XXXXXXX,      XXXXXXX
     ),
     [SYMNAV] = LAYOUT_split_4x8(
         KC_TAB,     _______,    _______,    KC_LBRC,    KC_RBRC,    _______,    XXXXXXX,    XXXXXXX,/**/XXXXXXX,    XXXXXXX,    _______,    _______,    _______,        _______,    _______,    _______,
